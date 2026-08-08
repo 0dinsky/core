098ff1fb11e6575e965aff7ccfa1658699877054
0dinsky <alex.v.ledenev234@gmail.com>
2026-08-08 12:18:28 +0000

    Update decrypt.rs

--- src/decrypt.rs
+++ src/decrypt.rs
@@ -158,7 +158,7 @@ async fn decrypt_session_key_symmetrically(
             // Finally, try decrypting using own AUTH tokens
             // There can be a lot of AUTH tokens,
             // because a new one is generated every time a QR code is shown
-            let res: Option<PlainSessionKey> = try_decrypt_with_auth_token(esk, conn, self_fp)?;
+            let res: Option<PlainSessionKey> = try_decrypt_with_auth_token(esk, conn, &self_fp)?;
             if let Some(plain_session_key) = res {
                 return Ok((plain_session_key, None));
             }
